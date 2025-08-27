c37a8f8c3d18880211deb396c2c28cb9c2923686
Mohamed Baslib <m.baslib517@gmail.com>
2025-08-27 23:58:27 +0300

    update src images

--- src/components/ContainerProducts.js
+++ src/components/ContainerProducts.js
@@ -5,52 +5,52 @@ export default function ContainerProducts({ addProduct }) {
 
   const productsData = [
     {
-      image: "images/product1.jpg",
+      image: `${process.env.PUBLIC_URL}/images/product1.jpg`,
       productName: "سامسونج S25 Ultra",
       productPrice: 1500,
     },
     {
-      image: "images/product2.jpg",
+      image: `${process.env.PUBLIC_URL}/images/product2.jpg`,
       productName: "ايفون 14 برو ماكس",
       productPrice: 1100,
     },
     {
-      image: "images/product3.jpg",
+      image: `${process.env.PUBLIC_URL}/images/product3.jpg`,
       productName: "ماك بوك برو 16",
       productPrice: 2500,
     },
     {
-      image: "images/product4.jpg",
+      image: `${process.env.PUBLIC_URL}/images/product4.jpg`,
       productName: "سماعة راس",
       productPrice: 100,
     },
     {
-      image: "images/product5.jpg",
+      image: `${process.env.PUBLIC_URL}/images/product5.jpg`,
       productName: "شاحن باور بانك",
       productPrice: 30,
     },
     {
-      image: "images/product6.jpg",
+      image: `${process.env.PUBLIC_URL}/images/product6.jpg`,
       productName: "سامسونح تاب S8 Ultra",
       productPrice: 1000,
     },
     {
-      image: "images/product7.jpg",
+      image: `${process.env.PUBLIC_URL}/images/product7.jpg`,
       productName: "بلاستيشن 5",
       productPrice: 500,
     },
     {
-      image: "images/product8.jpg",
+      image: `${process.env.PUBLIC_URL}/images/product8.jpg`,
       productName: "شاشة سامسونج 25انش",
       productPrice: 600,
     },
     {
-      image: "images/product9.jpg",
+      image: `${process.env.PUBLIC_URL}/images/product9.jpg`,
       productName: "ساعة ابل",
       productPrice: 450,
     },
     {
-      image: "images/product10.jpg",
+      image: `${process.env.PUBLIC_URL}/images/product10.jpg`,
       productName: "كاميرا كانون",
       productPrice: 2000,
     },
